443150f14b4194679bb87c5f570ba9d3ceeacbb1
박건욱 <geon-uk@baggeon-ug-ui-Macmini.local>
2021-02-05 23:03:51 +0900

    my first project

---
+++ slack-clone-ios-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slack-clone-ios-app
+//
+//  Created by 박건욱 on 2021/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slack-clone-ios-app/slack_clone_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  slack_clone_ios_appApp.swift
+//  slack-clone-ios-app
+//
+//  Created by 박건욱 on 2021/02/05.
+//
+
+import SwiftUI
+
+@main
+struct slack_clone_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
